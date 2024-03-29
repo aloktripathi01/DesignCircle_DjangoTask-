6b9be0cc9b0ff00fddc398675d5f142cf7e5545f
Alok Tripathi <54761280+aloktripathi01@users.noreply.github.com>
2024-03-29 11:53:58 +0530

    Update views.py

--- MyTaskAssignment/MyTask/views.py
+++ MyTaskAssignment/MyTask/views.py
@@ -11,7 +11,7 @@ class TaskPagination(PageNumberPagination):
     # setting the number of items in each page like it will show 10 items only  
     page_size = 10  
     page_size_query_param = 'page_size'  
-    max_page_size = 100  
+    max_page_size = 50  
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
